675edcd89a970e17af5dd235ced40c52c1f7e172
change <och8808@gmail.com>
2023-08-30 09:51:09 +0900

    Post 모델링

--- insta/settings.py
+++ insta/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 업로드한 사진을 저장할 위치
+MEDIA_ROOT = BASE_DIR / 'media' 
+
+# 미디어 경로를 처리할 URL
+MEDIA_URL = '/media/'
